aed94b365e9b3fb7dbe31de755e1c3b41a6e1abb
Maji <majisyogo0414@gmail.com>
2025-11-05 14:34:21 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 class Program
 {
     static void Main()
     {
-        // サンプル入力（#付き）
+        //【TEST】
         string input1 = "44 444 0 444 777 666 66 0 7777 666 333 8 9 2 777 33#";
         string input2 = "66 444 222 33 0 8 666 0 6 33 33 8 0 999 666 88#";
         string input3 = "333 777 666 6 0 7777 44 666 4 666 0 6 2 5 444 6 2#";
